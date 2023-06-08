131c6c3c82813cadbcb4a517912db911c56af06e
Vladislav <102545184+lezver@users.noreply.github.com>
2023-06-08 21:53:22 +0300

    x

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,61 +11,52 @@ export class App extends Component {
     filter: '',
   };
 
-  // handleSearch = ({ currentTarget: { value } }) =>
-  //   this.setState({ filter: value });
+  handleSearch = ({ currentTarget: { value } }) =>
+    this.setState({ filter: value });
 
-  // handleAddContact = contact => {
-  //   const { contacts } = this.state;
+  handleAddContact = contact => {
+    const { contacts } = this.state;
 
-  //   if (contacts.find(({ name }) => name === contact.name)) {
-  //     return Notiflix.Notify.failure(`${contact.name} is already in contacts.`);
-  //   } else {
-  //     this.setState({
-  //       contacts: [...contacts, contact],
-  //     });
-  //     Notiflix.Notify.success('You have a new contact!');
-  //   }
-  // };
+    if (contacts.find(({ name }) => name === contact.name)) {
+      return Notiflix.Notify.failure(`${contact.name} is already in contacts.`);
+    } else {
+      this.setState({
+        contacts: [...contacts, contact],
+      });
+      Notiflix.Notify.success('You have a new contact!');
+    }
+  };
 
-  // handleFilter = () => {
-  //   const { contacts, filter } = this.state;
+  handleFilter = () => {
+    const { contacts, filter } = this.state;
 
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-  // };
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
-  // handleDeleteContact = contactId => {
-  //   const { contacts } = this.state;
+  handleDeleteContact = contactId => {
+    const { contacts } = this.state;
 
-  //   this.setState({ contacts: contacts.filter(({ id }) => id !== contactId) });
-  // };
+    this.setState({ contacts: contacts.filter(({ id }) => id !== contactId) });
+  };
 
   render() {
-    // const { contacts } = this.state;
+    const { contacts } = this.state;
 
-    // if (contacts.length === 0) Notiflix.Notify.info('No contacts!');
+    if (contacts.length === 0) Notiflix.Notify.info('No contacts!');
     return (
       <section className="phonebook">
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm handleAddContact={this.handleAddContact} />
         <h2>Contacts</h2>
-        <Filter />
+        <Filter handleSearch={this.handleSearch} />
 
-        <ContactList />
+        <ContactList
+          contacts={this.handleFilter()}
+          handleDeleteContact={this.handleDeleteContact}
+        />
       </section>
     );
   }
 }
-
-//<section className="phonebook">
-//  <h1>Phonebook</h1>
-//  <ContactForm handleAddContact={this.handleAddContact} />
-//  <h2>Contacts</h2>
-//  <Filter handleSearch={this.handleSearch} />
-//
-//  <ContactList
-//    contacts={this.handleFilter()}
-//    handleDeleteContact={this.handleDeleteContact}
-//  />
-//</section>;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,15 @@
-// import './Filter.scss';
-// import PropTypes from 'prop-types';
+import './Filter.scss';
+import PropTypes from 'prop-types';
 
-// export const Filter = ({ handleSearch }) => {
-//   return (
-//     <label className="phonebook__search">
-//       Find contact by name:
-//       <input type="text" name="filter" onChange={handleSearch} />
-//     </label>
-//   );
-// };
+export const Filter = ({ handleSearch }) => {
+  return (
+    <label className="phonebook__search">
+      Find contact by name:
+      <input type="text" name="filter" onChange={handleSearch} />
+    </label>
+  );
+};
 
-// Filter.propTypes = {
-//   handleSearch: PropTypes.func.isRequired,
-// };
-
-export const Filter = () => {
-  return <p>Filter</p>;
+Filter.propTypes = {
+  handleSearch: PropTypes.func.isRequired,
 };
