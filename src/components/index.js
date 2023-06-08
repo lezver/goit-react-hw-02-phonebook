3decf31523f5561bae45f6fcbbf63ad8118fa615
Vladislav <102545184+lezver@users.noreply.github.com>
2023-06-08 21:15:34 +0300

    x

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import { App } from './App';
-import { ContactForm } from './ContactForm';
-import { Filter } from './Filter';
-import { ContactList } from './ContactList';
-import { startContacts } from './Backend';
+// import { ContactForm } from './ContactForm';
+// import { Filter } from './Filter';
+// import { ContactList } from './ContactList';
+// import { startContacts } from './Backend';
 
-export { App, ContactForm, Filter, ContactList, startContacts };
+export { App };
